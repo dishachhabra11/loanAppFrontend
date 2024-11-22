776a3479d7f2e265ad1cc46613b5d76ba8175d45
disha <dishachhabra173@gmail.com>
2024-11-22 09:17:12 +0530

    latest build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react({
+      jsxImportSource: "@emotion/react", // Use Emotion's JSX runtime
+      babel: {
+        plugins: ["@emotion/babel-plugin"], // Enable Emotion Babel plugin
+      },
+    }),
+  ],
+});
